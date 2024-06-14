b1ac76ff3a213cf733c37ae4fdf7569d3e03d111
tarikkazan <tarik.kazan@d-teknoloji.com.tr>
2024-06-14 11:54:32 +0300

    added naviigation

--- app/src/main/java/com/example/booknote/MainActivity.kt
+++ app/src/main/java/com/example/booknote/MainActivity.kt
@@ -3,39 +3,25 @@ package com.example.booknote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.booknote.presentation.books.BooksPage
+import com.example.booknote.presentation.notes.NotesPage
+import com.example.booknote.presentation.util.Page
 import com.example.booknote.ui.theme.BookNoteTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,50 +33,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(title = {
-                                Text(text = "Kitaplarım")
-                            })
-                        },
-                        floatingActionButton = {
-                            FloatingActionButton(
-                                onClick = {},
-                                shape = CircleShape,
-                                content = { Icon(Icons.Filled.Add, contentDescription = "Add") },
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = Page.BooksPage.route
+                    ){
+                        composable(route = Page.BooksPage.route){
+                            BooksPage(navController)
+                        }
+                        composable(
+                            route = Page.NotesPage.route +
+                                    "?bookId={bookId}&bookTitle={bookTitle}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "bookId"
+                                ) {
+                                    type = NavType.LongType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "bookTitle"
+                                ) {
+                                    type = NavType.StringType
+                                    defaultValue = ""
+                                }
                             )
-                        },
-                    ) { paddingValues ->
-                        paddingValues
-                        Column(
-                            modifier = Modifier.padding(top = 56.dp),
-                        ) {
-                            var query by remember { mutableStateOf("") }
-                            var active by remember { mutableStateOf(false) }
-                            Row {
-                                SearchBar(
-                                    modifier = Modifier.padding(start = 16.dp),
-                                    query = query,
-                                    onQueryChange = { query = it },
-                                    onSearch = { newQuery ->
-                                        println("Search on: $newQuery")
-                                    },
-                                    active = false,
-                                    onActiveChange = { active = it },
-                                    placeholder = {
-                                        Text(text = "Search")
-                                    },
-                                    leadingIcon = {
-                                        Icon(imageVector = Icons.Filled.Search, contentDescription = "searchIcon")
-                                    },
-                                    content = {},
-                                )
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowDropDown,
-                                    contentDescription = "Sort Icon",
-                                )
-                            }
-                            BooksGrid()
+                        ){
+                            val bookId = it.arguments?.getLong("bookId") ?: -1
+                            val bookTitle = it.arguments?.getString("bookTitle") ?: ""
+                            NotesPage(
+                                navController = navController,
+                                bookId = bookId,
+                                bookTitle = bookTitle
+                            )
                         }
                     }
                 }
@@ -99,26 +75,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun BooksGrid() {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
-        content = {
-            items(16) {
-                Image(
-                    modifier = Modifier.size(150.dp),
-                    painter = painterResource(id = R.drawable.blue_book),
-                    contentDescription = "Books Grid",
-                )
-            }
-        },
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BookNoteTheme {
-        BooksGrid()
+
     }
 }
