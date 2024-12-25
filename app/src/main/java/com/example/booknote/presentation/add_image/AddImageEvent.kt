a936fa879806e7c57384609ec83c5d921007db60
Ahtar1 <ahmettarkazan@gmail.com>
2024-12-25 23:54:59 +0300

    deleted Image Page

--- app/src/main/java/com/example/booknote/presentation/add_image/AddImageEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.booknote.presentation.add_image
-
-import com.example.booknote.domain.model.Note
-
-sealed class AddImageEvent {
-    data class AddImage(val note: Note): AddImageEvent()
-}
