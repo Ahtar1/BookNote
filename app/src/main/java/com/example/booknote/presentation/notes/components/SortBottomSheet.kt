30908d13060437f006e09745d0dc68623f720dbc
Ahtar1 <ahmettarkazan@gmail.com>
2025-05-08 12:23:44 +0300

    changed SortBottomSheet UI

--- app/src/main/java/com/example/booknote/presentation/notes/components/SortBottomSheet.kt
+++ app/src/main/java/com/example/booknote/presentation/notes/components/SortBottomSheet.kt
@@ -1,27 +1,23 @@
 package com.example.booknote.presentation.notes.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.booknote.domain.util.SortOrder
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SortBottomSheet(
-    onDismissRequest: () -> Unit,
+change    onDismissRequest: () -> Unit,
     sortItems: List<ToggleItem>,
     initialSelectedItem: ToggleItem?,
     onItemSelected: (ToggleItem) -> Unit,
@@ -32,29 +28,24 @@ fun SortBottomSheet(
     selectedItem = initialSelectedItem
 
     ModalBottomSheet(
+        modifier = Modifier
+            .navigationBarsPadding(),
         onDismissRequest = { onDismissRequest() },
     ) {
 
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+        ) {
             items(sortItems) { item ->
                 ToggleListItem(item, selectedItem?.id) { selectedId ->
                     selectedItem = sortItems.find { it.id == selectedId }
 
                     sortItems.forEach { it.isChecked = it.id == selectedItem?.id }
+                    selectedItem?.let { onClick(it.sortOrder) }
+                    onItemSelected(selectedItem!!)
                 }
             }
-        }
-        ElevatedButton(
-            onClick = {
-                selectedItem?.let { onClick(it.sortOrder) }
-                onItemSelected(selectedItem!!)
-            },
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(bottom = 16.dp)
-                .align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "Sort")
         }
     }
 }
